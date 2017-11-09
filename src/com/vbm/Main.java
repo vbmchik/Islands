77e3ace48231bf1fbf11a5b6677c2d1f7f5ec81b
vbmchik@gmail.com <38gjgeuftd>
2017-11-09 13:57:39 +0300

    Initial commit

---
+++ src/com/vbm/Main.java
@@ -0,0 +1,66 @@
+package com.vbm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[][] islands = new int[7][];
+        islands[0] = new int[]{1, 1, 0, 0, 0, 0, 0};
+        islands[1] = new int[]{1, 1, 3, 0, 2, 0, 0};
+        islands[2] = new int[]{0, 3, 1, 6, 0, 1, 0};
+        islands[3] = new int[]{0, 0, 6, 1, 0, 0, 1};
+        islands[4] = new int[]{0, 2, 0, 0, 1, 0, 0};
+        islands[5] = new int[]{0, 0, 1, 0, 0, 1, 2};
+        islands[6] = new int[]{0, 0, 0, 1, 0, 2, 1};
+
+        System.out.println("Source graph array: ");
+
+        arrayprint(islands);
+
+        arrayprint(mapper(islands));
+    }
+
+    public static void boardprint(int length, boolean numbers) {
+        if (numbers) {
+            for (int i = 0; i < length; ++i)
+                System.out.print("  " + String.valueOf(i+1) + " ");
+            System.out.println();
+        }
+        for (int i = 0; i < length; ++i)
+            System.out.print("----");
+        System.out.print("-");
+        System.out.println();
+    }
+
+    public static void arrayprint( int [][] islands )
+    {
+        boardprint(islands.length, true);
+        for (int i = 0; i < islands.length; ++i) {
+            System.out.print("| ");
+            for (int j = 0; j < islands.length; ++j)
+                System.out.print(islands[i][j] + " | ");
+            System.out.println("  " + String.valueOf(i + 1));
+            boardprint(islands.length, false);
+        }
+
+    }
+
+    public static int[][] mapper( int[][] graph ){
+        int [][] myMap = graph;
+           for( int i = 0 ; i < myMap.length ; ++ i ){
+               for( int j = 0 ; j < myMap.length ; ++ j ){
+                   if( i == j ) continue;
+                   if( myMap[i][j] != 0 ){
+                       for( int k = 0 ; k < myMap.length ; k++ ){
+                           if( k == i || k == j ) continue;
+                           if( myMap[j][k] == 0 ) continue;
+                           myMap[i][k] = ( myMap[i][k] > ( myMap[i][j] + myMap[j][k] ) ) || myMap[i][k] == 0 ? myMap[i][j] + myMap[j][k] : myMap[i][k];
+                           myMap[k][i] = myMap[i][k];
+                       }
+                   }
+               }
+           }
+        return myMap;
+    }
+
+}
